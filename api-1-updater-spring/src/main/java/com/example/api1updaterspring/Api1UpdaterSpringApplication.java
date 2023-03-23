d99cd1d02a71ca5a98e42ed0488a381be59e9a46
Casey Sayre <casey-sayre@users.noreply.github.com>
2023-03-23 00:25:09 +0000

    init

---
+++ api-1-updater-spring/src/main/java/com/example/api1updaterspring/Api1UpdaterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.api1updaterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api1UpdaterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api1UpdaterSpringApplication.class, args);
+	}
+
+}
---
+++ api-1-updater-spring/src/main/java/com/example/api1updaterspring/SocketTextHandler.java
@@ -0,0 +1,43 @@
+package com.example.api1updaterspring;
+
+import java.io.IOException;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+
+@Component
+public class SocketTextHandler extends TextWebSocketHandler {
+
+  List<WebSocketSession> sessions = new CopyOnWriteArrayList<WebSocketSession>();
+
+  @Override
+  public void afterConnectionEstablished(WebSocketSession session) throws java.lang.Exception {
+    System.out.println("afterConnectionEstablished");
+    sessions.add(session);
+  }
+
+  @Override
+  public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws java.lang.Exception {
+    System.out.println("afterConnectionClosed");
+    sessions.remove(session);
+  }
+
+  @Override
+  public void handleTextMessage(WebSocketSession session, TextMessage message)
+      throws InterruptedException, IOException {
+
+    String payload = message.getPayload();
+    System.out.println(String.format("handleTextMessage() received payload '%s'", payload));
+    JSONObject jsonObject = new JSONObject(payload);
+    session.sendMessage(new TextMessage("Hi " + jsonObject.get("user") + " how may we help you?"));
+  }
+
+}
